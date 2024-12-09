ba7fbe55b31a4437cc015432d79e0ee00b48942c
Giorgio Lovato <giorgio.lovato99@gmail.com>
2024-12-09 13:04:39 +0100

    1.2.0: repetition check

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
           pkgs = import nixpkgs {
             inherit system;
           };
-          version = "1.0.0";  
+          version = "1.2.0";  
         in 
         with pkgs;
         {
